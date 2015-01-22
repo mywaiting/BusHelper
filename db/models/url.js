1f0fb49d1f02a81051872ef0d7ba1cc2703c089b
turtleLin <930030895@qq.com>
2015-01-22 20:28:20 +0800

    url模板

---
+++ db/models/url.js
@@ -0,0 +1,11 @@
+/**
+ * Created by root on 15-1-22.
+ */
+var Url = {
+    baidu:{
+        hostname:'api.map.baidu.com',
+        searcherPath:"/place/v2/search?ak=T1E0HUvqvtFAsQMlP5il4ZgE&output=json&query=公交站&scope=1&location=location_X,location_Y&radius=2000"
+    }
+};
+
+module.exports = Url;
